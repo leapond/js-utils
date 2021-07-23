102495d8c777571d0da17c71d36f25d495260acf
arlen <dev@arlen.cn>
2021-07-24 06:41:51 +0800

    make Leapond Classes not be copy/merged

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const toStr = Object.prototype.toString
 const mergeTypes = {'[object Object]': 1, '[object Array]': 2, '[object Set]': 3, '[object Map]': 4}
 
 function getMergeType(target) {
-  return mergeTypes[toStr.call(target)] || 0
+  return target &&
+      // make Leapond Classes not be copy/merged
+      !('CID' in target) &&
+      mergeTypes[toStr.call(target)] ||
+      0
 }
 
 export {toStr, getMergeType}
