d0641d449b0db1b597147d339a1f0e3f11a6f85c
arlen <dev@arlen.cn>
2021-07-23 10:12:55 +0800

    return 0 for unsupported types.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const toStr = Object.prototype.toString
 const mergeTypes = {'[object Object]': 1, '[object Array]': 2, '[object Set]': 3, '[object Map]': 4}
 
 function getMergeType(target) {
-  return mergeTypes[toStr.call(target)]
+  return mergeTypes[toStr.call(target)] || 0
 }
 
 export {toStr, getMergeType}
